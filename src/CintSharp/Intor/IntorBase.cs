667f1492c6fd6e101f18e8a2274aaa1a4f022e40
则轩 刘 <liuzexuan02@hnu.edu.cn>
2025-03-15 16:15:33 +0800

    使用释放模式管理IntroOpter指针

--- src/CintSharp/Intor/IntorBase.cs
+++ src/CintSharp/Intor/IntorBase.cs
@@ -24,6 +24,7 @@ namespace CintSharp.Intor
         protected int Components { get; }
 
         protected IntorUtils.IntorType type;
+        private bool disposedValue;
 
         public IntorBase(CIntEnvs envs, string intorName, bool shouldOpt = true)
         {
@@ -43,13 +44,36 @@ namespace CintSharp.Intor
 
         public abstract Tensor<double> Invoke();
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                //if (disposing)
+                //{
+                //    // TODO: 释放托管状态(托管对象)
+                //}
+
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                if (ShouldOptimize && optimizer != nint.Zero)
+                {
+                    LibcintHandler.ReleaseOptimizer(ref optimizer);
+                }
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        ~IntorBase()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: false);
+        }
+
         public void Dispose()
         {
-            if(ShouldOptimize && optimizer != nint.Zero) 
-            {
-                LibcintHandler.ReleaseOptimizer(ref optimizer);
-                optimizer = nint.Zero;
-            }
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
     }
